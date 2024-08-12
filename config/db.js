7d410d1b5c9a0c17b611e41a6266d9234c9cc392
Kapil Sengar <kapilsengar@Kapils-Laptop.local>
2024-08-12 17:04:20 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import colors from "colors";
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
+
+//
+//mongodb+srv://WEB_DATA:KAPIL%40ATLAS49@cluster0.zkvz0on.mongodb.net/TEST
